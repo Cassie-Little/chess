8603bb2448c5c61a617a840956e40650cfbd8e2a
Cassie-Little <ecass12@byu.edu>
2024-01-22 16:09:35 -0700

    implemented getPromotion

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -11,11 +11,13 @@ import java.util.Objects;
 public class ChessMove {
     private final ChessPosition start;
     private final ChessPosition end;
+    private final ChessPiece.PieceType promotion;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
         this.start = startPosition;
         this.end = endPosition;
+        this.promotion = promotionPiece;
 
 
     }
@@ -35,24 +37,16 @@ public class ChessMove {
     }
 
     @Override
-    public String toString() {
-        return "ChessMove{" +
-                "start=" + start +
-                ", end=" + end +
-                '}';
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ChessMove chessMove = (ChessMove) o;
-        return Objects.equals(start, chessMove.start) && Objects.equals(end, chessMove.end);
+        return Objects.equals(start, chessMove.start) && Objects.equals(end, chessMove.end) && promotion == chessMove.promotion;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(start, end);
+        return Objects.hash(start, end, promotion);
     }
 
     /**
@@ -62,18 +56,9 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        return null;
+        return promotion;
         //throw new RuntimeException("Not implemented");
     }
 
-    private void canBePromoted(ChessBoard board, ChessPosition myPosition, ChessPiece myPiece) {
-        if (myPiece.getPieceType() == ChessPiece.PieceType.PAWN
-                && myPiece.getTeamColor() == ChessGame.TeamColor.WHITE
-                && myPosition.getRow() == 8) {
-            ChessPiece.QueenMoves(board, myPosition, myPiece);
-        }
 
-
-        throw new RuntimeException("not implemented");
-    }
 }
