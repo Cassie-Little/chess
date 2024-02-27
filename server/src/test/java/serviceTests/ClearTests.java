cf80a2754fbe96d36c3edbbf0c595fbe806d6fcf
Cassie-Little <ecass12@byu.edu>
2024-02-27 11:55:15 -0700

    commiting to run the autograder

--- server/src/test/java/serviceTests/ClearTests.java
+++ server/src/test/java/serviceTests/ClearTests.java
@@ -1,6 +1,7 @@
 package serviceTests;
 
 import dataAccess.*;
+import model.UserData;
 import org.junit.jupiter.api.Test;
 import service.GameService;
 import service.UserService;
@@ -9,8 +10,8 @@ public class ClearTests {
     @Test
     public void positiveClear() throws Exception{
         AuthDAO authDao = new MemoryAuthDAO();
-        UserDAO userDAO = new MemoryUserDAO();
-        GameDAO gameDAO = new MemoryGameDAO();
-        //var newUser = UserService.register();
+        UserData person = new UserData("username",null, null);
+
+
     }
 }
