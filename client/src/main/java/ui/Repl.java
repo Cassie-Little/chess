7b0dd824a8876b7e1c03613415371997f8717fc1
Cassie-Little <ecass12@byu.edu>
2024-03-16 19:02:33 -0600

    worked on repl, client, ServerFacade

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,40 @@
+package ui;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    public Repl(String serverUrl) {
+        final Client client;
+
+        client = new Client(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.print(Client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = Client.eval(line);
+                System.out.print(SET_BG_COLOR_DARK_GREEN + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " +SET_TEXT_COLOR_MAGENTA);
+    }
+
+
+}
